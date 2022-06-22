trying to fix numerical error

src/final_plots.py
@@ -13,38 +13,48 @@ def final_plot(self,iter,image_path,method):
 
 	ltype = '-x'
 	color = 'b'
-	# plot E
+
+	# plot E_T & R_T
 	f, (ax1,ax2) = plt.subplots(2,1)
 	
-	E = torch.Tensor(self.E)
+	E_T = torch.Tensor(self.E_T)
+	R_T = torch.Tensor(self.R_T)
 	ax1.set_yscale('symlog')
-	ax1.plot(torch.linspace(0,iter+1,iter+2),E[:,0].numpy(),ltype,color=color)
-	ax1.set_title('E over iteration')
+	ax1.plot(torch.linspace(0,iter+1,iter+2),E_T.numpy(),ltype,color=color)
+	ax1.set_title('E_T over iteration')
 	ax1.set_xlabel('Iteration')
-	ax1.set_ylabel('e(x)')
+	ax1.set_ylabel('E_T')
 
 	ax2.set_yscale('symlog')
-	ax2.plot(torch.linspace(0,iter+1,iter+2),E[:,1].numpy(),ltype,color=color)
+	ax2.plot(torch.linspace(0,iter+1,iter+2),R_T.numpy(),ltype,color=color)
 	ax1.set_xlabel('Iteration')
-	ax1.set_ylabel('e(t)')
+	ax1.set_ylabel('R_T')
 
-	f.savefig(image_path+'/E.jpg',bbox_inches='tight')
+	f.savefig(image_path+'/estimation/estimation of T.jpg',bbox_inches='tight')
 
-	# plot R
-	f, (ax1,ax2) = plt.subplots(2,1)
-	R = torch.Tensor(self.R)
-	ax1.set_yscale('symlog')
-	ax1.plot(torch.linspace(0,iter+1,iter+2),R[:,0].numpy(),ltype,color=color)
-	ax1.set_title('R over iteration')
-	ax1.set_xlabel('Iteration')
-	ax1.set_ylabel('r(x)')
+	# plot theta
+	E_theta = torch.Tensor(self.E_theta)
+	R_theta = torch.Tensor(self.R_theta)
 
-	ax2.set_yscale('symlog')
-	ax2.plot(torch.linspace(0,iter+1,iter+2),R[:,1].numpy(),ltype,color=color)
-	ax1.set_xlabel('Iteration')
-	ax1.set_ylabel('r(t)')
+	f, ax1 = plt.subplots(1,3)
+	for idx,ax in enumerate(ax1):
+		ax.set_yscale('symlog')
+		ax.plot(torch.linspace(0,iter+1,iter+2),E_theta[:,idx].numpy(),ltype,color=color)
+		ax.set_xlabel('Iteration')
+		ax.set_ylabel('E_theta')
+	ax.set_title('E_theta over iteration')
 
-	f.savefig(image_path+'/R.jpg',bbox_inches='tight')
+	f.savefig(image_path+'/estimation/E_theta.jpg',bbox_inches='tight')
+
+	f, ax1 = plt.subplots(1,3)
+	for idx,ax in enumerate(ax1):
+		ax.set_yscale('symlog')
+		ax.plot(torch.linspace(0,iter+1,iter+2),R_theta[:,idx].numpy(),ltype,color=color)
+		ax.set_xlabel('Iteration')
+		ax.set_ylabel('R_theta')
+	ax.set_title('R_theta over iteration')
+
+	f.savefig(image_path+'/estimation/R_theta.jpg',bbox_inches='tight')
 
 	# plot M
 	plt.figure()
@@ -54,15 +64,16 @@ def final_plot(self,iter,image_path,method):
 	plt.title('Misfit')
 	plt.savefig(image_path+'/Misfit.jpg',bbox_inches='tight')
 
-	# plot
-	plt.figure()
-	w_exact = self.w_exact 
-	m1 = self.m1
-	plt.scatter(w_exact[0:100,0],w_exact[0:100,1], s=5, c='b', marker='o')
-	plt.scatter(m1[0:100,0],m1[0:100,1], s=5, c='r', marker='o')
-	plt.title('Reconstruction of the control')
-	plt.xlabel('x')
-	plt.ylabel('t')
-	plt.savefig(image_path+'/ReconstructionOfControl.jpg',bbox_inches='tight')
+	# plot reconstruction of theta
+	f, ax1 = plt.subplots(1,3)
+	theta_hat = self.theta_hat
+	for idx,ax in enumerate(ax1):
+		ax.set_yscale('symlog')
+		ax.plot(torch.linspace(0,iter+1,iter+2),theta_hat[:,idx].numpy(),'k-')
+		ax.plot(torch.linspace(0,iter+1,iter+2),(self.theta[idx])*torch.ones(iter+2),'r-')
+		ax.set_xlabel('Iteration')
+		ax.set_ylabel('theta_hat[%d]'%(idx))
+	ax.set_title('Reconstruction of theta')
+	plt.savefig(image_path+'/ReconstructionOfTheta.jpg',bbox_inches='tight')
 
 	return

src/set_up_ensemble.py
@@ -1,7 +1,5 @@
-from sympy import true
 import torch
 from src import moments
-import torch.linalg as linalg
 
 # def is_sorted(x):
 # 	sorted,_ = diag(x).sort(descending=true)
@@ -27,7 +25,8 @@ def set_up_ensemble(self,ensembleSize,initEnsemble):
 			En[j,:] = torch.mean(T[j,:]) * torch.ones(ensembleSize) + torch.randn(ensembleSize)
 		
 		self.En = En
-		# self.m1,self.m2 = moments.moments(self.En)
+		self.m1,self.m2 = moments.moments(self.En)
+		(self.theta_hat).append(self.m1[:3])
 
 		return
 

src/simulation/set_up_model.py
@@ -84,10 +84,10 @@ def set_up_model(self,image_path):
 	# compute Tk
 	for i in range(1,Nt+1):
 		T[:,i] = torch.matmul(A,T[:,i-1])+r2*u[:,i-1]
-		if i == int(Nt/2):
-			print("after control:")
-		if i%5:
-			print(torch.norm(T[:,i]))
+		# if i == int(Nt/2):
+		# 	print("after control:")
+		# if i%5:
+		# 	print(torch.norm(T[:,i]))
 
 	# plot T without noise
 	def animate(k):
@@ -118,7 +118,7 @@ def set_up_model(self,image_path):
 	else:
 		gamma = torch.eye(l)
 		noise = torch.zeros(l,Nt+1)
-
+	
 	y = T + noise
 
 

src/update/convergence.py
@@ -1,42 +1,41 @@
 from torch import *
 import torch.linalg as linalg
-from src.predict_by_En import predict_by_En
+from src.compute_Atheta import compute_Atheta
 
-def convergence(self):
-	#En,u_exact,m1,G,p,noise,gamma
-	En = self.En # l*2*ensemblesize
-	w_exact = self.w_exact #l*2
-	m1 = mean(En,dim=2) #l*2
-	# G = self.G
-	# p = self.p
-	# noise = self.noise
+def convergence(self,iter):
+
+	En = self.En 
+	m1 = self.m1
+	noise = self.noise[:,iter]
 	# gamma = self.gamma
-	observations = self.observations # N_control+1 * Nt_control+1
+	T = self.T[:,iter]
+	theta = self.theta
 
-	e = En - m1[:,:,None] # l*2*ensembleSize
-	r = En - w_exact[:,:,None] # l*2*ensembleSize
+	Atheta = compute_Atheta(theta,self.N,self.Nt)
+	(self.theta_hat).append(m1[:3])
 
-	misfit = self.predict_by_En(En) - observations[:,:,None]
-	# N_control+1 * Nt_control+1 *ensembleSize
-	# misfit = mm(G,r) - noise[:,None]
+	e_theta = En[:3,:]-m1[:3,None]
+	e_T = En[3:,:] - m1[3:,None] 
+	r_theta = En[:3,:] - theta[:,None]
+	r_T = En[3:,:] - T[:,None]
 
-	Eix = sum(pow(linalg.vector_norm(e[:,0,:],dim=0),2))\
-		/ (linalg.vector_norm(m1[:,0])**2)
-	Eix = Eix/self.ensembleSize
-	Eit = sum(pow(linalg.vector_norm(e[:,1,:],dim=0),2))\
-		/ (linalg.vector_norm(m1[:,1])**2)
-	Eit = Eit/self.ensembleSize
-	(self.E).append([Eix,Eit])
+	misfit = mm(Atheta,r_T) - noise[:,None]
 
+	E_theta = div(mean(e_theta**2,dim=1),m1[:3]**2)
+	(self.E_theta).append(E_theta)
 
-	Rix = sum(pow(linalg.vector_norm(r[:,0,:],dim=0),2))\
-		/ (linalg.vector_norm(w_exact[:,0])**2)
-	Rix = Rix/self.ensembleSize
-	Rit = sum(pow(linalg.vector_norm(r[:,1,:],dim=0),2))\
-		/ (linalg.vector_norm(w_exact[:,1])**2)
-	Rit = Rit/self.ensembleSize
-	(self.R).append([Rix,Rit])
+	E_T = sum(pow(linalg.vector_norm(e_T,dim=0),2))\
+		/ (linalg.vector_norm(m1[3:])**2)
+	E_T = E_T/self.ensembleSize
+	(self.E_T).append(E_T)
 
+	R_theta = div(mean(r_theta**2,dim=1),theta**2)
+	(self.R_theta).append(R_theta)
+
+	R_T = sum(pow(linalg.vector_norm(r_T,dim=0),2))\
+		/ (linalg.vector_norm(T)**2)
+	R_T = R_T/self.ensembleSize
+	(self.R_T).append(R_T)
 
 	# ae = mm(mm(sqrt(linalg.pinv(gamma)),G),e)
 	# AEi = sum(pow(linalg.vector_norm(ae,dim=0),2))\
@@ -48,7 +47,6 @@ def convergence(self):
 	# 	/ (linalg.vector_norm(p)**2)
 	# (self.AR).append(ARi/self.ensembleSize)
 
-	misfit = flatten(misfit,start_dim=0,end_dim=1)
 	Mi = sum(pow(linalg.vector_norm(misfit,dim=0),2))
 	(self.M).append(Mi/self.ensembleSize)
 